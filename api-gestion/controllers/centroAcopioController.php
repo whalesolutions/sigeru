500d92220731eaf28551fef32ae18e26344ef907
RODRIGO MORELLI LOPEZ <49383843@ceibal.edu.uy>
2026-07-23 07:41:55 -0300

    Add management API

---
+++ api-gestion/controllers/centroAcopioController.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . "/../models/centroacopio.php";
+
+class CentroAcopioController
+{
+    private CentroAcopio $modelo;
+
+    public function __construct()
+    {
+        $this->modelo = new CentroAcopio();
+    }
+
+    public function listar(): array
+    {
+        return $this->modelo->obtenerTodas();
+    }
+
+    public function buscar(int $id): ?array
+    {
+        return $this->modelo->obtenerPorId($id);
+    }
+
+    public function crear(array $datos): array
+    {
+        return $this->modelo->crear($datos);
+    }
+
+    public function actualizar(int $id, array $datos): ?array
+    {
+        return $this->modelo->actualizar($id, $datos);
+    }
+
+    public function eliminar(int $id): bool
+    {
+        return $this->modelo->eliminar($id);
+    }
+}
